35bc807beab98d2977ba4e5ee9ebc68e944355e2
Noah Caldwell <noah.caldwell@journalism.cuny.edu>
2016-02-10 18:50:15 -0500

    add initial commit

---
+++ js.js
@@ -0,0 +1,14 @@
+var fifty = 50;
+var hundred = 100;
+
+var addition = fifty + hundred;
+var subtraction = hundred - fifty;
+var multiplication = hundred * fifty;
+var division = hundred / fifty;
+
+document.getElementById("addition").innerHTML = "When I add " + hundred + " to " + fifty + " I get " + addition + ".";
+document.getElementById("subtraction").innerHTML = "When I subtract " + fifty + " from " + hundred + " I get " + subtraction + ".";
+document.getElementById("multiplication").innerHTML = "When I multiply " + hundred + " and " + fifty + " I get " + multiplication + ".";
+document.getElementById("division").innerHTML = "When I divide " + hundred + " by " + fifty + " I get " + division + ".";
+
+
